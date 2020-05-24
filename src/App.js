ce57b482b42e5501d2e4d50a98fc08fdac1f17dc
Kushagra Johari <skjkushagra1309@gmail.com>
2020-05-24 16:54:57 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Using React set environment
         </a>
       </header>
     </div>
